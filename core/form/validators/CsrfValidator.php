4c06fcfb629f8067d2bc0235c3147319faa62bbd
goph-R <gopher.hu@gmail.com>
2019-12-08 00:46:54 +0100

    CsrfValidator added.

---
+++ core/form/validators/CsrfValidator.php
@@ -0,0 +1,26 @@
+<?php
+
+class CsrfValidator extends Validator {
+    
+    /** @var UserSession */
+    private $userSession;
+    private $userSessionName = '';
+    
+    /** @var Form */
+    private $form;
+    private $inputName;
+
+    public function __construct(Framework $framework, $userSessionName, Form $form, $inputName) {
+        parent::__construct($framework);
+        $this->message = $this->translation->get('validator', 'not_valid_csrf');
+        $this->userSession = $framework->get('userSession');
+        $this->userSessionName = $userSessionName;
+        $this->form = $form;
+        $this->inputName = $inputName;
+    }
+
+    protected function doValidate($value) {
+        return $this->form->getValue($this->inputName) == $this->userSession->get($this->userSessionName);
+    }
+
+}
